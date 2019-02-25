f2de15e50e676e0f8cf93b9e02f4f2cb904e79da
Travis Frazier <31898316+travisfrazier@users.noreply.github.com>
2019-02-25 15:57:17 -0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -30,3 +30,15 @@ const image = new Vue({
 		}
 	}
 });
+35.240544;
+function initMap() {
+	// The location of Uluru
+	var uluru = { lat: 35.240544, lng: -80.813173 };
+	// The map, centered at Uluru
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 14,
+		center: uluru
+	});
+	// The marker, positioned at Uluru
+	var marker = new google.maps.Marker({ position: uluru, map: map });
+}
