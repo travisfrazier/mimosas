36bc0e7a5c778416962eed1b32b942376a50a229
Travis Frazier <31898316+travisfrazier@users.noreply.github.com>
2019-02-25 14:46:45 -0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,6 @@ const mobileMenu = document.querySelector('.mobile__menu');
 const mobileNav = document.querySelector('.mobile__nav');
 const appButton = document.querySelector('.appointment');
 
-//App Button
-window.addEventListener('scroll', function(event) {
-	event.preventDefault();
-	appButton.style.display = 'block';
-});
-
-//listen here
-document
-	.querySelector('.home__images')
-	.addEventListener('mouseover', function(event) {
-		event.preventDefault();
-
-		imageShow.style.backgroundImage = `url(${event.target.src}`;
-		console.log(event.target);
-	});
 //Vue JS Code
 const nav = new Vue({
 	el: '#nav',
@@ -30,6 +15,18 @@ const nav = new Vue({
 		openNav: function() {
 			this.showNav = !this.showNav;
 			this.isChanged = !this.isChanged;
+		}
+	}
+});
+
+const image = new Vue({
+	el: '#imageToggle',
+	data: {
+		src: '../images/salon-three.png'
+	},
+	methods: {
+		toggle: function() {
+			this.src = event.target.src;
 		}
 	}
 });
